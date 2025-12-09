56c8124dd609a03bf6d641a88c6dd58c9377cadd
thisisanro <antonromanenko.me@gmail.com>
2025-12-09 13:10:30 +0000

    add local server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/thisisanro/chirpy
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := http.Server{
+		Handler: mux,
+		Addr:    ":8080",
+	}
+
+	server.ListenAndServe()
+}
